72f511b4f38405c05088c7439876e06c9274346d
!! <65474747+rockysmart1@users.noreply.github.com>
2025-04-24 19:52:32 +1000

    comment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOp.java
@@ -164,7 +164,8 @@ public class BasicOp extends LinearOpMode {
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
             telemetry.update();
         }
-    }}
+    }
+}
 //yayy it finally worked!
 //,more stuff
 //lalala
